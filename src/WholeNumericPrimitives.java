2ff55c6355819221e096a9bccfba642eda430543
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-07-04 15:26:30 -0400

    Corrected code display for cleanliness

--- src/WholeNumericPrimitives.java
+++ src/WholeNumericPrimitives.java
@@ -27,7 +27,7 @@ public class WholeNumericPrimitives {
         int secondBin = 0b10111; // 7 decimal
         int sumBin = firstBin + secondBin; //  16 decimal, 10000 binary
 
-        System.out.print("First binary = " + firstBin + " | Second binary = " + secondBin);
+        System.out.println("First binary = " + firstBin + " | Second binary = " + secondBin);
         System.out.println("Decimal sum = " + sumBin + " Binary Sum = " + Integer.toBinaryString(sumBin));
 
     }
